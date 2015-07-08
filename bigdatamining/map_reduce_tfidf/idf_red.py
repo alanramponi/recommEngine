1cd34f5beead47ef0a4f49f1b3039fb15aa53f32
rakwaht <Martintoni@MacBook-Pro-di-Martintoni.local>
2015-07-08 15:57:30 +0200

    IDF calculation fixed

--- bigdatamining/map_reduce_tfidf/idf_red.py
+++ bigdatamining/map_reduce_tfidf/idf_red.py
@@ -13,37 +13,37 @@ for line in sys.stdin:
     
 	tot_item = int(os.environ["TOT_LINE"])
 
-    	line = line.strip()
-    	word, value = line.split("\t")
+    line = line.strip()
+    word, value = line.split("\t")
 
 	asin, tf = value.split("=")	
 
-    	# if this is the first iteration
-    	if not target_key:
-        	target_key = word
+    # if this is the first iteration
+    if not target_key:
+        target_key = word
     
-    	# if they're the same, log it
-    	if word == target_key:
-		temp_counter[asin] = word
-        	item_count += 1
-    	else:
-        	# state change (previous line was k=x, this line is k=y)
+    # if they're the same, log it
+    if word == target_key:
+        temp_counter[asin] = tf
+        item_count += 1
+    else:
+        # state change (previous line was k=x, this line is k=y)
 		for k,v in temp_counter.iteritems():
-			new_key = str(v) + "@" + k 
-			tfidf = float(tf) * (float(item_count)/tot_item)
+			new_key = target_key + "@" + k
+			tfidf = float(v) * log((float(tot_item)/item_count), 10)
 			result = [new_key, tfidf]
-        		print("\t".join(str(v) for v in result))		
+            print("\t".join(str(v) for v in result))
 		
 		# reset for next stage
 		item_count = 1
 		temp_counter = {}
 		temp_counter[asin] = word
-        	target_key = word
+        target_key = word
 
 # this is to catch the final counts after all records have been received.
 for k,v in temp_counter.iteritems():
 	new_key = str(v) + "@" + k 
 	tfidf = float(tf) * (float(item_count)/tot_item)
 	result = [new_key, tfidf]
-        print("\t".join(str(v) for v in result))
+    print("\t".join(str(v) for v in result))
 
--- bigdatamining/map_reduce_tfidf/tfidf_red.py
+++ bigdatamining/map_reduce_tfidf/tfidf_red.py
@@ -10,27 +10,27 @@ new_value = []
 for line in sys.stdin:
     
     line = line.strip()
-        key, tfidf = line.split("\t")
+    key, tfidf = line.split("\t")
         
-        asin, word = key.split("@")
+    asin, word = key.split("@")
         
-        # if this is the first iteration
-        if not target_key:
-            target_key = asin
+    # if this is the first iteration
+    if not target_key:
+        target_key = asin
     
-        # if they're the same, log it
-        if asin == target_key:
-            v = word + "=" + tfidf
-            new_value.append(v)
-        else:
-            result_value = "@".join(str(v) for v in new_value)
-            result = [asin, result_value]
-            print("\t".join(str(v) for v in result))
-            # reset for next stage
-            target_key = asin
-            new_value = []
-            v = word + "=" + tfidf
-            new_value.append(v)
+    # if they're the same, log it
+    if asin == target_key:
+        v = word + "=" + tfidf
+        new_value.append(v)
+    else:
+        result_value = "@".join(str(v) for v in new_value)
+        result = [asin, result_value]
+        print("\t".join(str(v) for v in result))
+        # reset for next stage
+        target_key = asin
+        new_value = []
+        v = word + "=" + tfidf
+        new_value.append(v)
 
 # this is to catch the final counts after all records have been received.
 result_value = "@".join(str(v) for v in new_value)
